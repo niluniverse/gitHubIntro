432c268bbb56f628cc284d09792e6ccb9241d79d
niluniverse <niluniverse@gmail.com>
2019-09-22 15:26:29 +0530

    second commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+	    Console.WriteLine("The current time is "+ DateTime.Now);
+
+        }
+    }
+}
